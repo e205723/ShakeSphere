0f69a499c16d0989689d1e727b7870ea2bee2baa
e205723 <e205723@ie.u-ryukyu.ac.jp>
2022-07-22 02:22:46 +0900

    feat(web): lock y axis rotation of blender models

--- react/shake-sphere/src/three/ArmeriaViewer.tsx
+++ react/shake-sphere/src/three/ArmeriaViewer.tsx
@@ -1,6 +1,5 @@
 import { useRef, Suspense } from 'react';
-import { useLoader, Canvas } from '@react-three/fiber';
-// import { useLoader, Canvas, useFrame } from '@react-three/fiber';
+import { useLoader, Canvas, useFrame } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Mesh } from 'three';
 import { OrbitControls } from '@react-three/drei';
@@ -31,26 +30,37 @@ const GltfModel = function createGltfModel(arg: GltfModelArg) {
   );
 };
 
-const ArmeriaViewer = function createModelViewer({
-  scale = 1.5,
-  position = [0, -3.3, 0],
-  rotation = [Math.PI / 15, 0, 0],
+const ArmeriaViewer = function createModelViewer({ scale, position, rotation }: {
+  scale: number;
+  position: number[];
+  rotation: number[];
 }) {
   return (
     <Canvas>
       <ambientLight intensity={0.5} />
       <Suspense fallback={null}>
         <GltfModel modelPath="/armeria_s.glb" scale={scale} position={position} rotation={rotation} />
-        <OrbitControls />
+        <OrbitControls
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          maxDistance={5}
+          minDistance={5}
+        />
       </Suspense>
     </Canvas>
   );
 };
 
 ArmeriaViewer.propTypes = {
-  scale: PropTypes.number.isRequired,
-  position: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  rotation: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+  scale: PropTypes.number,
+  position: PropTypes.arrayOf(PropTypes.number.isRequired),
+  rotation: PropTypes.arrayOf(PropTypes.number.isRequired),
+};
+
+ArmeriaViewer.defaultProps = {
+  scale: 1.5,
+  position: [0, -3.5, 0],
+  rotation: [0, 0, 0],
 };
 
 export default ArmeriaViewer;
--- react/shake-sphere/src/three/CompassViewer.tsx
+++ react/shake-sphere/src/three/CompassViewer.tsx
@@ -1,5 +1,5 @@
 import { useRef, Suspense } from 'react';
-import { useLoader, Canvas } from '@react-three/fiber';
+import { useLoader, Canvas, useFrame } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Mesh } from 'three';
 import { OrbitControls } from '@react-three/drei';
@@ -16,6 +16,9 @@ const GltfModel = function createGltfModel(arg: GltfModelArg) {
   const ref = useRef<Mesh>(null);
   const gltf = useLoader(GLTFLoader, arg.modelPath);
 
+  useFrame(() => {
+    ref.current!.rotation.y += 0.01;
+  });
   return (
     <primitive
       ref={ref}
@@ -27,26 +30,39 @@ const GltfModel = function createGltfModel(arg: GltfModelArg) {
   );
 };
 
-const CompassViewer = function createModelViewer({
-  scale = 0.5,
-  position = [0, 0, 0],
-  rotation = [Math.PI / 2, 0, 0],
+const CompassViewer = function createModelViewer({ scale, position, rotation }: {
+  scale: number;
+  position: number[];
+  rotation: number[];
 }) {
   return (
     <Canvas>
       <ambientLight intensity={0.5} />
       <Suspense fallback={null}>
         <GltfModel modelPath="/compass.glb" scale={scale} position={position} rotation={rotation} />
-        <OrbitControls />
+        <OrbitControls
+          maxAzimuthAngle={0}
+          minAzimuthAngle={0}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          maxDistance={5}
+          minDistance={5}
+        />
       </Suspense>
     </Canvas>
   );
 };
 
 CompassViewer.propTypes = {
-  scale: PropTypes.number.isRequired,
-  position: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-  rotation: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+  scale: PropTypes.number,
+  position: PropTypes.arrayOf(PropTypes.number.isRequired),
+  rotation: PropTypes.arrayOf(PropTypes.number.isRequired),
+};
+
+CompassViewer.defaultProps = {
+  scale: 0.5,
+  position: [0, 0, 0],
+  rotation: [Math.PI / 2, 0, 0],
 };
 
 export default CompassViewer;
